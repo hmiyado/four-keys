6b988591b1ec80c3a5084910ddbf7b283a6c11f7
miyado <10195648+hmiyado@users.noreply.github.com>
2022-06-29 23:08:29 +0900

    add QueryReleases

--- internal/releases/query_tags.go
+++ internal/releases/query_tags.go
@@ -1,11 +1,35 @@
 package releases
 
 import (
+	"fmt"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
+
+type Release struct {
+	Tag  string
+	Date time.Time
+}
+
+func (r *Release) String() string {
+	return fmt.Sprintf("(Tag=%v,Date=%v)", r.Tag, r.Date)
+}
+
+func QueryReleases(repository *git.Repository) []*Release {
+	tags := QueryTags(repository)
+	releases := make([]*Release, len(tags))
+	for i := 0; i < len(tags); i++ {
+		tag := tags[i]
+		commit, _ := repository.CommitObject(tag.Hash())
+		releases[i] = &Release{Tag: tag.Name().Short(), Date: commit.Author.When}
+	}
+	sort.Slice(releases, func(i, j int) bool { return releases[i].Date.Before(releases[j].Date) })
+	return releases
+}
 
 func QueryTags(repository *git.Repository) []*plumbing.Reference {
 	itr, _ := repository.Tags()
--- internal/releases/query_tags_test.go
+++ internal/releases/query_tags_test.go
@@ -1,19 +1,94 @@
 package releases
 
 import (
+	"os"
+	"sort"
 	"testing"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
-func TestQueryTags(t *testing.T) {
-	r, _ := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+var r, emptyRepository *git.Repository
+
+func TestMain(m *testing.M) {
+	r, _ = git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
 		URL: "https://github.com/go-git/go-git",
 	})
+	emptyRepository, _ = git.Init(memory.NewStorage(), nil)
 
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func TestQueryReleasesShouldHaveSameCountToTags(t *testing.T) {
+	releases := QueryReleases(r)
+	expectedReleasesNum := len(QueryTags(r))
+
+	if len(releases) != expectedReleasesNum {
+		for i := 0; i < len(releases); i++ {
+			if releases[i] == nil {
+				t.Logf("releases[%v] = nil", i)
+			} else {
+				t.Logf("releases[%d] = %s", i, releases[i].String())
+			}
+		}
+		t.Errorf("num of tags should be %d but %d", expectedReleasesNum, len(releases))
+	}
+}
+
+func TestQueryReleasesShouldBeSortedByDate(t *testing.T) {
+	releases := QueryReleases(r)
+
+	if sort.SliceIsSorted(releases, func(i, j int) bool { return releases[i].Date.Before(releases[j].Date) }) {
+		return
+	}
+	for i := 0; i < len(releases); i++ {
+		if releases[i] == nil {
+			t.Logf("releases[%v] = nil", i)
+		} else {
+			t.Logf("releases[%d] = %s", i, releases[i].String())
+		}
+	}
+	t.Errorf("releases are not sorted")
+}
+
+func TestQueryReleasesShouldReturnEmptyForEmptyRepository(t *testing.T) {
+	releases := QueryReleases(emptyRepository)
+
+	if len(releases) == 0 {
+		return
+	}
+	for i := 0; i < len(releases); i++ {
+		if releases[i] == nil {
+			t.Logf("releases[%v] = nil", i)
+		} else {
+			t.Logf("releases[%d] = %s", i, releases[i].String())
+		}
+	}
+	t.Errorf("releases are not sorted")
+}
+
+func TestQueryTagsShouldHaveTags(t *testing.T) {
 	tags := QueryTags(r)
 	expectedTagNum := 60
+
+	if len(tags) != expectedTagNum {
+		for i := 0; i < len(tags); i++ {
+			if tags[i] == nil {
+				t.Logf("tags[%d] = nil", i)
+			} else {
+				t.Logf("tags[%d] = %s", i, tags[i].String())
+			}
+		}
+		t.Errorf("num of tags should be %d but %d", expectedTagNum, len(tags))
+	}
+}
+
+func TestQueryTagsShouldReturnEmptyForEmptyRepository(t *testing.T) {
+	tags := QueryTags(emptyRepository)
+	expectedTagNum := 0
 
 	if len(tags) != expectedTagNum {
 		for i := 0; i < len(tags); i++ {
