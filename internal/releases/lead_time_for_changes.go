4f6784ddc96a2ef4f1db5d84091c22139d6585b1
miyado <10195648+hmiyado@users.noreply.github.com>
2022-07-12 21:33:13 +0900

    Consider first release when get lead time for changes

--- internal/releases/lead_time_for_changes.go
+++ internal/releases/lead_time_for_changes.go
@@ -7,9 +7,33 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
+// GetLeadTimeForChanges returns duration after older commit is created (exlusive) and until newer commit is created (inclusive).
+// if commit1 xor commit2 is nil, it is assumed that older commit is the initial commit of this repository.
+// if commit1 and commit2 is nil, return duration of 0.
 func GetLeadTimeForChanges(repository *git.Repository, commit1 *object.Commit, commit2 *object.Commit) *time.Duration {
+	if commit1 == nil && commit2 == nil {
+		zero := time.Duration(0)
+		return &zero
+	}
+	if commit1 == commit2 {
+		zero := time.Duration(0)
+		return &zero
+	}
 	var ancestor, child *object.Commit
-	if isAncestor, _ := commit1.IsAncestor(commit2); isAncestor {
+	if commit1 == nil || commit2 == nil {
+		iter, err := repository.Log(&git.LogOptions{Order: git.LogOrderCommitterTime})
+		if err != nil {
+			return nil
+		}
+		var initialCommit *object.Commit
+		iter.ForEach(func(c *object.Commit) error { initialCommit = c; return nil })
+		ancestor = initialCommit
+		if commit1 == nil {
+			child = commit2
+		} else {
+			child = commit1
+		}
+	} else if isAncestor, _ := commit1.IsAncestor(commit2); isAncestor {
 		ancestor = commit1
 		child = commit2
 	} else if isAncestor, _ := commit2.IsAncestor(commit1); isAncestor {
--- internal/releases/lead_time_for_changes_test.go
+++ internal/releases/lead_time_for_changes_test.go
@@ -61,6 +61,29 @@ func TestGetLeadTimeForChangesShouldReturn0ForSameCommits(t *testing.T) {
 	t.Error(duration, "should be equal to ", 0)
 }
 
+func TestGetLeadTimeForChangesShouldRunWithInitialCommit(t *testing.T) {
+	// https://github.com/go-git/go-git/releases/tag/v1.0.0
+	commit1_0_0, err1 := repository.CommitObject(plumbing.NewHash("6f43e8933ba3c04072d5d104acc6118aac3e52ee"))
+	if err1 != nil {
+		t.Error(err1.Error())
+	}
+
+	duration := GetLeadTimeForChanges(repository, nil, commit1_0_0)
+	expected, _ := time.ParseDuration("4152h5m44s")
+	if duration.Minutes() == expected.Minutes() {
+		return
+	}
+	t.Error(duration, "should be equal to ", expected)
+}
+
+func TestGetLeadTimeForChangesShouldReturn0WhenCommitsAreNil(t *testing.T) {
+	duration := GetLeadTimeForChanges(repository, nil, nil)
+	if duration.Minutes() == 0 {
+		return
+	}
+	t.Error(duration, "should be equal to ", 0)
+}
+
 // isNearBy checks actual is in range of [expected*(1-epsilon), expected*(1+epsiolon)]
 func isNearBy(actual float64, expected float64, epsilon float64) bool {
 	return actual >= expected*(1-epsilon) && actual <= expected*(1+epsilon)
