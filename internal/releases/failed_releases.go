c26caa8697d2555301bcfed3c71d3e0b9640b962
miyado <10195648+hmiyado@users.noreply.github.com>
2022-07-17 19:39:57 +0900

    add isRestoredRelease to calculate time to restore service

---
+++ internal/releases/failed_releases.go
@@ -0,0 +1,26 @@
+package releases
+
+import (
+	"strings"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
+)
+
+// isResoredRelease returns true if newerCommit is restored.
+// If a commit containing 'hotfix' exists between newerCommit and olderCommit, the release is considered restored.
+// It returns nil if newer commit is null.
+// It returns nil if older commit is not ancestor of newer.
+// If older commit is null, it is assumed that older commit is the initial commit of this repository.
+func isRestoredRelease(repository *git.Repository, olderCommit *object.Commit, newerCommit *object.Commit) bool {
+	isRestored := false
+	traverseCommits(repository, olderCommit, newerCommit, func(c *object.Commit) error {
+		if strings.Contains(c.Message, "hotfix") {
+			isRestored = true
+			return storer.ErrStop
+		}
+		return nil
+	})
+	return isRestored
+}
