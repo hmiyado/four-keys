d56ad9988db9b07941e588da9de79e0f8baf4e00
miyado <10195648+hmiyado@users.noreply.github.com>
2022-07-17 17:26:43 +0900

    Extract traverse commits function

--- internal/releases/lead_time_for_changes.go
+++ internal/releases/lead_time_for_changes.go
@@ -13,43 +13,22 @@ import (
 // It returns nil if older commit is not ancestor of newer.
 // If older commit is null, it is assumed that older commit is the initial commit of this repository.
 func GetLeadTimeForChanges(repository *git.Repository, olderCommit *object.Commit, newerCommit *object.Commit) *time.Duration {
-	if newerCommit == nil {
-		return nil
-	}
 	if olderCommit != nil && olderCommit.Hash == newerCommit.Hash {
 		zero := time.Duration(0)
 		return &zero
 	}
-	var ancestor, child *object.Commit
-	child = newerCommit
-	if olderCommit == nil {
-		iter, err := repository.Log(&git.LogOptions{Order: git.LogOrderCommitterTime})
-		if err != nil {
-			return nil
-		}
-		var initialCommit *object.Commit
-		iter.ForEach(func(c *object.Commit) error { initialCommit = c; return nil })
-		ancestor = initialCommit
-	} else {
-		ancestor = olderCommit
-	}
-	if isAncestor, _ := ancestor.IsAncestor(child); !isAncestor {
+
+	var lastCommit *object.Commit
+	err := traverseCommits(repository, olderCommit, newerCommit, func(c *object.Commit) error {
+		lastCommit = c
 		return nil
-	}
-	afterAncestor := ancestor.Committer.When.AddDate(0, 0, 1)
-	iter, err := repository.Log(&git.LogOptions{
-		From:  child.Hash,
-		Since: &afterAncestor,
-		Order: git.LogOrderCommitterTime,
 	})
 	if err != nil {
 		return nil
 	}
-	var lastCommit *object.Commit
-	iter.ForEach(func(c *object.Commit) error {
-		lastCommit = c
+	if lastCommit == nil {
 		return nil
-	})
-	duration := child.Committer.When.Sub(lastCommit.Committer.When)
+	}
+	duration := newerCommit.Committer.When.Sub(lastCommit.Committer.When)
 	return &duration
 }
