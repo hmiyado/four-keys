f9a9f18e9413c5bd03cdd4bbdccfea6461380b8a
miyado <10195648+hmiyado@users.noreply.github.com>
2022-07-21 22:47:19 +0900

    Move QueryTag to git_util

--- internal/releases/query_tags.go
+++ internal/releases/query_tags.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
@@ -127,26 +126,4 @@ func QueryReleases(repository *git.Repository, option *Option) []*Release {
 		})
 	}
 	return releases
-}
-
-func QueryTags(repository *git.Repository) []*plumbing.Reference {
-	itr, _ := repository.Tags()
-	tags := make([]*plumbing.Reference, 0)
-
-	itr.ForEach(func(ref *plumbing.Reference) error {
-		// refs/tags/xxx
-		// lightweight tag
-		if strings.Split(ref.Name().String(), "/")[1] == "tags" {
-			tags = append(tags, ref)
-			return nil
-		}
-
-		_, err := repository.TagObject(ref.Hash())
-		if err != nil {
-			tags = append(tags, ref)
-		}
-		return nil
-	})
-
-	return tags
 }
